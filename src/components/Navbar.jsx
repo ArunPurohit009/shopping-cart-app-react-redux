cf1c56d386808391f9ab09b60db5c0c46ee4e134
Arun Purohit <arunpurohit234@gmail.com>
2025-08-24 16:48:29 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,20 @@
+import {FaShoppingCart} from "react-icons/fa"
+import { NavLink } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (<div>
+     
+     <div>
+        <NavLink to="/"><img src="../../public/favicon.ico" alt="navbar" /></NavLink>
+        <NavLink to="/"><p>
+          Home
+        </p></NavLink>
+    <NavLink to="/cart"><div><FaShoppingCart/></div></NavLink>
+        
+       
+
+  </div> 
+  </div>);
 };
 
 export default Navbar;
